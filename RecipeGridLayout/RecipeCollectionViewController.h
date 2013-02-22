cdb609112ee8f2a999901d378d5ba9251f6415ff
Andre Trosky <spinninghalf@gmail.com>
2013-02-22 16:59:11 +1100

    first commit yo

---
+++ RecipeGridLayout/RecipeCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeCollectionViewController.h
+//  RecipeGridLayout
+//
+//  Created by andre trosky on 22/02/13.
+//  Copyright (c) 2013 andre trosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeCollectionViewController : UICollectionViewController
+
+@end
---
+++ RecipeGridLayout/RecipeCollectionViewController.m
@@ -0,0 +1,38 @@
+//
+//  RecipeCollectionViewController.m
+//  RecipeGridLayout
+//
+//  Created by andre trosky on 22/02/13.
+//  Copyright (c) 2013 andre trosky. All rights reserved.
+//
+
+#import "RecipeCollectionViewController.h"
+
+@interface RecipeCollectionViewController ()
+
+@end
+
+@implementation RecipeCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
